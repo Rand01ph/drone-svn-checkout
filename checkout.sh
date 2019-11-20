b65d72059baa712a34bd388a17333ac3fb900f57
Rand01ph <tanyawei1991@gmail.com>
2019-11-20 13:37:28 +0800

    add svn co version

--- checkout.sh
+++ checkout.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 URL=${PLUGIN_URL:-$URL}
+VERSION=${PLUGIN_VERSION:-"HEAD"}
 OURPUT_PATH=${PLUGIN_OUTPUT_PATH:-$DRONE_WORKSPACE}
 USERNAME=${PLUGIN_USERNAME:-$USERNAME}
 PASSWORD=${PLUGIN_PASSWORD:-$PASSWORD}
 
 svn checkout ${URL} ${OURPUT_PATH} \
+    --revision ${VERSION} \
     --username ${USERNAME} \
     --password ${PASSWORD} \
     --no-auth-cache \
